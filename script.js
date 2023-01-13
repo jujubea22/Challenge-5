54b8b93122bec2ad018f56c2278ffad31f3e3f63
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-01-13 17:32:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("mm, dd, yy"));
 
+    $('.saveBtn').on('click', function(){
+      
+        var time = $(this).parent().attr('id');
+        var input = $(this).siblings('.description').val()
+     
+        localStorage.setItem(input, time);
+    });
+   
+function timeStop()   {
